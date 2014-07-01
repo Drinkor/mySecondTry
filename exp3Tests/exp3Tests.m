db560c2e6bdff718188bc869fa79cea375b45016
itisioslab <itisioslab@Mac-mini-administrator-77.local>
2014-07-01 13:27:19 +0400

    Initial Commit

---
+++ exp3Tests/exp3Tests.m
@@ -0,0 +1,34 @@
+//
+//  exp3Tests.m
+//  exp3Tests
+//
+//  Created by itisioslab on 01.07.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface exp3Tests : XCTestCase
+
+@end
+
+@implementation exp3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
